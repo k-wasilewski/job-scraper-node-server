e878caf8469af416f233600c4093b3752997fdc7
Wasilewski <jakub.wasilewski.ctr@sabre.com>
2022-01-22 14:59:47 +0100

    restruct

--- next-graphql-server/pages/api/graphql.js
+++ next-graphql-server/pages/api/graphql.js
@@ -1,6 +1,6 @@
 import  { ApolloServer }  from  "apollo-server-micro";
-import  { typeDefs } from "../schemas";
-import  { resolvers }  from  "../resolvers";
+import  { typeDefs } from "../../schemas";
+import  { resolvers }  from "../../resolvers";
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
