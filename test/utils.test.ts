bc625a086a8d8f872499b51f7dbe6647378c60dd
Wasilewski <e-jbww@gft.com>
2024-03-01 13:37:22 +0100

    complete utils.test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,45 @@
-import { generateUUID, getPath, getWebpageName  } from "../src/utils";
+import fs from "fs";
+import { 
+    generateUUID,
+    getPath,
+    getWebpageName,
+    getDirectories,
+    getFilenames,
+    removeFile,
+    removeDir,
+    sameMembers,
+    getJobLinkByUuid
+} from "../src/utils";
+import { Job } from "../src/mongodb";
+
+jest.mock('fs', () => ({
+    __esModule: true,
+    default: {
+        readdirSync: jest.fn(),
+        statSync: jest.fn(),
+        unlink: jest.fn(),
+        rmdir: jest.fn()
+    }
+}));
+
+const mockJobs: Job[] = [
+    {
+        uuid: 'mockUuid1',
+        host: 'mockHost1',
+        path: 'mockPath1',
+        link: 'mockLink1',
+        userUuid: 'mockUserUuid1'
+    },
+    {
+        uuid: 'mockUuid2',
+        host: 'mockHost2',
+        path: 'mockPath2',
+        link: 'mockLink2',
+        userUuid: 'mockUserUuid2'
+    }
+];
+
+const mockFilenames = ['filename1.json', 'filename2.exe', 'dir1', 'filename3.png', '.dir2'];
 
 describe('utils spec', () => {
     it('generateUUID spec', () => {
@@ -20,11 +61,69 @@ describe('utils spec', () => {
         expect(ret).toEqual('bulldogjob');
     });
 
-    /*it('getDirectories', () => {
+    it('getDirectories', () => {
+        (fs.readdirSync as jest.Mock).mockImplementation((path) => mockFilenames);
+        (fs.statSync as jest.Mock).mockImplementation((path) => ({
+            isDirectory: jest.fn().mockImplementation(() => path.includes('dir'))
+        }));
+
         const ret = getDirectories('.');
 
-        // todo: mock 'fs' here
+        expect(ret).toEqual(expect.arrayContaining(['.dir2', 'dir1']));
+    });
 
-        console.log(ret)
-    });*/
+    it('getFilenames', () => {
+        (fs.readdirSync as jest.Mock).mockImplementation((path) => 
+            mockFilenames.filter(f => !f.includes('dir')));
+
+        const ret = getFilenames('.');
+
+        expect(ret).toEqual(expect.arrayContaining(['filename1', 'filename2', 'filename3']));
+    });
+
+    it('removeFile', () => {
+        jest.mocked(fs.unlink).mockImplementationOnce((path, cb) => true);
+
+        const ret1 = removeFile('./file');
+
+        expect(ret1).toBeTruthy();
+        
+        jest.mocked(fs.unlink).mockImplementationOnce((path, cb) => { cb(new Error())});
+
+        const ret2 = removeFile('./file');
+
+        expect(ret2).toBeFalsy();
+    });
+
+    it('removeDir', () => {
+        jest.mocked(fs.rmdir).mockImplementationOnce((path, cb) => true);
+
+        const ret1 = removeDir('./dir');
+
+        expect(ret1).toBeTruthy();
+        
+        jest.mocked(fs.rmdir).mockImplementationOnce((path, cb) => { cb(new Error())});
+
+        const ret2 = removeDir('./dir');
+
+        expect(ret2).toBeFalsy();
+    });
+
+    it.each([[['a', 'b'], ['b', 'a'], true], [['a', 'b'], ['b'], false],  [[], [], true], [[null], [null], true], [[undefined], [undefined], true], [[false], [undefined], false]])
+    ('sameMembers: %p', (l, r, res) => {
+        const ret = sameMembers(l as Object[], r as Object[]);
+
+        expect(ret).toBeDefined();
+        expect(ret).toEqual(res);
+    });
+
+    it('getJobLinkByUuid', () => {
+        const ret1 = getJobLinkByUuid(mockJobs[1].uuid!, mockJobs);
+
+        expect(ret1).toEqual(mockJobs[1].link);
+
+        const ret2 = getJobLinkByUuid('mockUuid3', mockJobs);
+
+        expect(ret2).toEqual(null);
+    });
 });
