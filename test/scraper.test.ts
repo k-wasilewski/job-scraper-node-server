7d7fa6135eea216aaafa93c07d8d5e7ac5b29215
Wasilewski <e-jbww@gft.com>
2024-03-08 13:04:09 +0100

    complete scraper.test

--- test/scraper.test.ts
+++ test/scraper.test.ts
@@ -1,5 +1,9 @@
 import { SPRING_SCRAPE_UUID } from "../src/auth";
 import { scrape } from "../src/scraper";
+import fs from 'fs';
+import puppeteer from "puppeteer";
+import * as mongodb from '../src/mongodb';
+import { pubsub } from "../src/resolvers";
 
 jest.mock('apollo-server-express', () => ({
     ApolloServer: jest.fn()
@@ -17,19 +21,48 @@ jest.mock('fs', () => ({
     }
 }));
 
+let i = 1;
+const mockScreenshot = jest.fn();
+const mockSetViewport = jest.fn();
+const mockEvaluate = jest.fn().mockImplementation(() => {
+    return i++ % 2 === 0 ? [] : ['/link1', '/jobLinkContains1', '/jobLinkContains2'];
+});
+const mockWaitForSelector = jest.fn();
+const mockGoto = jest.fn();
+const mockNewPage = jest.fn().mockImplementation(() => ({
+    goto: mockGoto,
+    waitForSelector: mockWaitForSelector,
+    evaluate: mockEvaluate,
+    setViewport: mockSetViewport,
+    screenshot: mockScreenshot
+}));
 jest.mock('puppeteer', () => ({
     __esModule: true,
     default: {
-        launch: () => ({
-            newPage: () => ({
-                goto: jest.fn(),
-                waitForSelector: jest.fn(),
-                evaluate: jest.fn().mockReturnValue(['link1', 'link2']),
-                setViewport: jest.fn(),
-                screenshot: jest.fn()
-            })
-        })
+        launch: jest.fn().mockImplementation(() => ({
+            newPage: mockNewPage
+        }))
     },
+}));
+
+let k = 0;
+jest.mock('../src/mongodb', () => ({
+    getJobByLink: jest.fn().mockImplementation(() => {
+        return k++ % 2 === 0 ? null : {
+            uuid: 'mockUuid',
+            host: 'mockHost',
+            path: 'mockPath',
+            link: 'mockLink',
+            userUuid: 'mockUserUuid'
+        }; 
+    }),
+    insertToJobs: jest.fn()   
+}));
+
+jest.mock('../src/resolvers', () => ({
+    pubsub: {
+        publish: jest.fn()
+    }
 }));
 
 describe('scraper spec', () => {
@@ -45,18 +78,37 @@ describe('scraper spec', () => {
         expect(res).toBeUndefined();
     });
 
-    it('scrape persists jobs to mongodb, saves screenshots, publishes events', async () => {
+    it('scrape persists jobs to mongodb, saves screenshots, publishes events', async () => {        
         const res = await scrape(
-            'host',
-            'path',
+            'http://host.com',
+            '/path/to/my/resource/{}',
             'jobAnchorSelector',
             'jobLinkContains',
-            1,
+            2,
             'userUuid'
         );
 
-        // todo
-
+        expect(puppeteer.launch).toHaveBeenCalled();
+        expect(mockWaitForSelector).toHaveBeenCalledTimes(2);
+        expect(mockWaitForSelector).toHaveBeenCalledWith('jobAnchorSelector');
+        expect(mockEvaluate).toHaveBeenCalledTimes(2);
+        expect(fs.mkdirSync).toHaveBeenCalledWith('./screenshots/userUuid/host', { recursive: true });
+        expect(mockNewPage).toHaveBeenCalledTimes(3);
+        expect(mockSetViewport).toHaveBeenCalledTimes(2);
+        expect(mockGoto).toHaveBeenCalledTimes(4);
+        expect(mockGoto).toHaveBeenCalledWith('http://host.com/path/to/my/resource/1');
+        expect(mockGoto).toHaveBeenCalledWith('http://host.com/path/to/my/resource/2');
+        expect(mockGoto).toHaveBeenCalledWith('http://host.com/jobLinkContains1');
+        expect(mockGoto).toHaveBeenCalledWith('http://host.com/jobLinkContains2');
+        expect(mongodb.getJobByLink).toHaveBeenCalledTimes(2);
+        expect(mongodb.getJobByLink).toHaveBeenCalledWith('userUuid', 'http://host.com/jobLinkContains1');
+        expect(mongodb.getJobByLink).toHaveBeenCalledWith('userUuid', 'http://host.com/jobLinkContains2'); 
+        expect(mongodb.insertToJobs).toHaveBeenCalledTimes(1);
+        expect(mongodb.insertToJobs).toHaveBeenCalledWith({ host: 'http://host.com', path: '/path/to/my/resource/{}', link: 'http://host.com/jobLinkContains1', userUuid: 'userUuid', uuid: expect.stringContaining('-') });
+        expect(mockScreenshot).toHaveBeenCalledTimes(1);
+        expect(mockScreenshot).toHaveBeenCalledWith({path: expect.stringMatching(/.\/screenshots\/userUuid\/host\/\w{8}-\w{4}-4\w{3}-\w{4}-\w{12}.png/), fullPage: true});
+        expect(pubsub.publish).toHaveBeenCalledTimes(1);
+        expect(pubsub.publish).toHaveBeenCalledWith('newJobs', { newJobs: { timestamp: new Date().toString(), link: 'http://host.com/jobLinkContains1' } });
         expect(res).toEqual({ complete: true });
     });
 });
